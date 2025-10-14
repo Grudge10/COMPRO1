849eec0646799c065a928ec696f91a502915fd6b
Grudge10 <100700937+Grudge10@users.noreply.github.com>
2025-10-14 09:01:06 +0800

    Update EvenNumber.java

--- EvenNumber.java
+++ EvenNumber.java
@@ -5,7 +5,15 @@ public class EvenNumber {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
+
+        if (!input.hasNextInt()) {
+            System.out.println("Invalid Choice!");
+            System.exit(0);
+        }
+
         int n = input.nextInt();
+
+        System.out.printf("The even numbers from 0 to %d", n);
 
         int x = 0;
         while (x <= n) {
@@ -14,5 +22,7 @@ public class EvenNumber {
             }
             x++;
         }
+
+        input.close();
     }
 }
