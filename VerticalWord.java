8b27b23c0f6f0ef90a481889a78ca74cd67f5617
Grudge10 <100700937+Grudge10@users.noreply.github.com>
2025-10-30 14:45:33 +0800

    Add files via upload

---
+++ VerticalWord.java
@@ -0,0 +1,10 @@
+public class VerticalWord {
+    public static void main(String[] args) {
+        String word = "Madam, I'm Adam"; // Given word
+
+        // Print the word vertically
+        for (int i = 0; i < word.length(); i++) {
+            System.out.println(word.charAt(i));
+        }
+    }
+}
