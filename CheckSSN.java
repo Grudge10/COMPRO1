a1f53ecc0f93eae0841b33f3a8c9113e839b6887
Grudge10 <100700937+Grudge10@users.noreply.github.com>
2025-12-04 07:12:50 +0000

    fixed

--- CheckSSN.java
+++ CheckSSN.java
@@ -11,15 +11,14 @@ public class CheckSSN {
 
     public static String validateSSN(String SSN) {
         if (SSN.length() == 11) {
-            for (int i = 0; i < 11;i++) {
-                if ((i == 0 || i == 1 || i == 2 || i == 4 || i == 5 || i == 7 || i == 8 || i == 9 || i == 10) && Character.isDigit(SSN.charAt(i)))
-                    return "a valid";
-                else
+            for (int i = 0; i < 11; i++) {
+                if ((i == 3 || i == 6) && SSN.charAt(i) != '-')
+                    return "an invalid";
+                if ((i == 0 || i == 1 || i == 2 || i == 4 || i == 5 || i == 7 || i == 8 || i == 9 || i == 10) && !Character.isDigit(SSN.charAt(i)))
                     return "an invalid";
             }
-        } else {
-            return "a invalid";
-        }
-        return "an invalid";
+            return "a valid";
+        } else 
+            return "an invalid";
     }
 }
