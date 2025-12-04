cddbf31f2952da6d073d527db6cefd8ecd6c8b23
Grudge10 <100700937+Grudge10@users.noreply.github.com>
2025-12-04 13:00:40 +0000

    compiled all three into one file

--- StringAndCharacterFunctionsActivity.java
+++ StringAndCharacterFunctionsActivity.java
@@ -1,5 +1,70 @@
-public class GenerateVehiclePlateNumbers {
+import java.util.Scanner;
+
+public class StringAndCharacterFunctionsActivity {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter two characters: ");
+        String choice = input.nextLine();
+        System.out.println(studentMajorAndStatus(choice));
+
+        System.out.print("Enter a SSN: ");
+        String SSN = input.nextLine();
+        System.out.printf("%s is %s social security number\n", SSN, validateSSN(SSN));
+        input.close();
+
+        generateVehiclePlateNumbers();
+    }
+
+    public static String studentMajorAndStatus(String choice) {
+        String majorAndStatus = "";
+        if (choice.charAt(0) == 'M' || choice.charAt(0) == 'C' || choice.charAt(0) == 'I') {
+            switch (choice.charAt(0)) {
+                case 'M':
+                    majorAndStatus += "Mathematics ";
+                    break;
+                case 'C':
+                    majorAndStatus += "Computer Science ";
+                    break;
+                case 'I':
+                    majorAndStatus += "Information Technology ";
+                    break;
+            }
+            if (choice.charAt(1) == '1' || choice.charAt(1) == '2' || choice.charAt(1) == '3' || choice.charAt(1) == '4') {
+                switch (choice.charAt(1)) {
+                    case '1':
+                        majorAndStatus += "Freshman";
+                        break;
+                    case '2':
+                        majorAndStatus += "Sophomore";
+                        break;
+                    case '3':
+                        majorAndStatus += "Junior";
+                        break;
+                    case '4':
+                        majorAndStatus += "Senior";
+                        break;
+                }
+            } else
+                return "Invalid input";
+        } else
+            return "Invalid input";
+        return majorAndStatus;
+    }
+
+    public static String validateSSN(String SSN) {
+        if (SSN.length() == 11) {
+            for (int i = 0; i < 11; i++) {
+                if ((i == 3 || i == 6) && SSN.charAt(i) != '-')
+                    return "an invalid";
+                if ((i == 0 || i == 1 || i == 2 || i == 4 || i == 5 || i == 7 || i == 8 || i == 9 || i == 10) && !Character.isDigit(SSN.charAt(i)))
+                    return "an invalid";
+            }
+            return "a valid";
+        } else
+            return "an invalid";
+    }
+
+    public static String generateVehiclePlateNumbers() {
         String plateNumber = "";
         for (int i = 1; i <= 3; i++) {
             int randomNumber = 1 + (int) (Math.random() * 26);
@@ -85,6 +150,6 @@ public class GenerateVehiclePlateNumbers {
             }
         }
         plateNumber += Integer.toString(1000 + (int) (Math.random() * 9999));
-        System.out.println(plateNumber);
+        return plateNumber;
     }
 }
