c4431c556a413458a66ed005b3016e65148e770c
Jan Liam Arias <100700937+Grudge10@users.noreply.github.com>
2026-01-13 05:52:36 +0000

    First ever project this second sem

---
+++ firstyr_secndsem/prelims/Array1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Array1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(num[i]);
+        }
+
+        System.out.print("Enter a number: ");
+        int choice = input.nextInt();
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] == choice) {
+                System.out.println("Found " + choice + " at index " + i);
+                break;
+            }
+            System.out.println(choice + " is not found in any index.");
+        }
+
+        input.close();
+    }
+}
